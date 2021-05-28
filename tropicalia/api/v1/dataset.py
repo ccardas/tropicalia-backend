e71f767a08975339fa67e75fcd19b17db7803f82
thecristian1409 <criscardas@gmail.com>
2021-05-28 19:33:46 +0200

    Dataset endpoint

--- tropicalia/api/v1/dataset.py
+++ tropicalia/api/v1/dataset.py
@@ -4,7 +4,7 @@ from tropicalia.auth import get_current_user
 from tropicalia.database import Database, get_connection
 from tropicalia.logger import get_logger
 from tropicalia.manager import DatasetManager
-from tropicalia.models.dataset import Dataset
+from tropicalia.models.dataset import Dataset, DatasetRow
 from tropicalia.models.user import UserInDB
 
 logger = get_logger(__name__)
@@ -31,3 +31,45 @@ async def get(
         raise HTTPException(status_code=404, detail="Specified data not found")
 
     return data
+
+
+@router.post(
+    "/data/upsert",
+    summary="Upsert data to DB",
+    tags=["data"],
+    response_model=DatasetRow,
+    response_description="Upsert data to DB",
+)
+async def upsert(
+    row: DatasetRow, current_user: UserInDB = Depends(get_current_user), db: Database = Depends(get_connection)
+) -> DatasetRow:
+    """
+    Inserts or updates existing row in the DB.
+    """
+    upsert_row = await DatasetManager().upsert(row, current_user.username, db)
+
+    if not upsert_row:
+        raise HTTPException(status_code=404, detail="Upsert data error")
+
+    return upsert_row
+
+
+@router.post(
+    "/data/delete",
+    summary="Delete row from DB",
+    tags=["data"],
+    response_model=DatasetRow,
+    response_description="Delete row from DB",
+)
+async def delete(
+    row: DatasetRow, current_user: UserInDB = Depends(get_current_user), db: Database = Depends(get_connection)
+) -> DatasetRow:
+    """
+    Deletes given row from the DB.
+    """
+    deleted_row = await DatasetManager().delete(row, current_user.username, db)
+
+    if not deleted_row:
+        raise HTTPException(status_code=404, detail="Delete row error")
+
+    return deleted_row
--- tropicalia/manager.py
+++ tropicalia/manager.py
@@ -12,7 +12,7 @@ class DatasetManager:
 
     async def get(self, crop_type: str, current_user: str, db: Database) -> Dataset:
         """
-        Retrieves specified data from the dataset in the DB.
+        Retrieves specified data from the dataset in the database.
         """
         logger.debug(f"User {current_user} has requested {crop_type} from the DB")
 
@@ -31,3 +31,76 @@ class DatasetManager:
         dataset_rows = [DatasetRow(**row) for row in rows]
 
         return Dataset(data=dataset_rows)
+
+    async def upsert(self, row: DatasetRow, current_user: str, db: Database) -> DatasetRow:
+        """
+        Inserts or updates a row in the database given its id.
+        """
+        logger.debug(f"User {current_user} has requested an update to the DB")
+
+        row_in_db = await self.find_one(row.uid, db)
+
+        if row_in_db:
+            query = f"""
+                UPDATE dataset
+                SET (date, crop_type, yield_values) =
+                ('{row.date}', '{row.crop_type}', '{row.yield_values}')
+                WHERE uid = {row.uid}
+            """
+        else:
+            query = f"""
+                INSERT INTO dataset (uid, date, crop_type, yield_values)
+                VALUES ({row.uid}, '{row.date}', '{row.crop_type}', '{row.yield_values}')
+            """
+
+        await self.execute(query, DatasetRow, db)
+
+        row_in_db = await self.find_one(row.uid, db)
+
+        if row_in_db == row:
+            return row_in_db
+
+    async def delete(self, row: DatasetRow, current_user: str, db: Database) -> DatasetRow:
+        """
+        Deletes a dataset entry in the database given its id.
+        """
+        logger.debug(f"User {current_user} has requested a row delete to the DB")
+
+        row_in_db = await self.find_one(row.uid, db)
+
+        if row_in_db:
+            query = f"""
+                DELETE FROM dataset
+                WHERE uid = {row.uid}
+            """
+            await self.execute(query, DatasetRow, db)
+
+            row_in_db = await self.find_one(row.uid, db)
+
+            if not row_in_db:
+                return row
+
+    async def find_one(self, uid: int, db: Database) -> DatasetRow:
+        """
+        Find a dataset entry in the database given its id.
+        """
+        query = f"""
+            SELECT * FROM dataset WHERE uid = {uid}
+        """
+        row = await self.execute(query, DatasetRow, db)
+
+        return row
+
+    async def execute(self, query: str, model: DatasetRow, db: Database) -> DatasetRow:
+        """
+        Given the database it executes the specified query and returns the involved DatasetRow model.
+        Only for single-row altering queries.
+        """
+        res = await db.execute(query)
+        row = await res.fetchall()
+
+        await db.commit()
+
+        if row:
+            row_dict = {key: row[0][t] for t, key in enumerate(model.__fields__.keys())}
+            return model(**row_dict)
